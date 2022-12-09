854f7c73c39c3ebfdcf69ba533a7d8e5dcb2116b
m-housni <mohamed.housni@um5s.net.ma>
2022-12-09 15:12:47 +0100

    about .ready(() => {...})

--- packages/web-integration-oc10/js/app.js
+++ packages/web-integration-oc10/js/app.js
@@ -1,3 +1,7 @@
+/**
+ document.ready is fired when the DOM is ready, but before all the assets are loaded. This is a good place to attach event handlers to elements that are already in the DOM. If you need to wait for all the assets to be loaded, use window.load instead. See https://learn.jquery.com/using-jquery-core/document-ready/ for more information.
+ */
+
 $(document).ready(function () {
   $('#apps')
     .find('li[data-id=web]')
