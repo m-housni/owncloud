760feb1f5f52ce413965d426b1da707cfbf2daa0
m-housni <mohamed.housni@um5s.net.ma>
2023-01-22 22:36:48 +0100

    added comments to  the cucumber.js

--- cucumber.js
+++ cucumber.js
@@ -1,11 +1,18 @@
+// path is a library that helps you work with file and directory paths
 const path = require('path')
+
+// config is a file that contains the configuration for the tests
 const { config } = require('./tests/e2e/config')
+
+// fs is a library that helps you work with the file system
 const fs = require('fs')
 
+// Create the report directory if it does not exist
 if (!fs.existsSync(config.reportDir)) {
   fs.mkdirSync(path.join(config.reportDir, 'cucumber'), { recursive: true })
 }
 
+// This is the configuration for the cucumber tests
 module.exports = {
   e2e: `
   --require ./tests/e2e/**/*.ts
